e6ab25f5a3bfcaea36926c19d1ec41a886a3511b
Sebastian Quack <sebastianquack@gmail.com>
2016-04-26 11:29:58 +0200

    remove moderator iframe view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,18 +6,8 @@ class HomeController < ApplicationController
   
   def set_settings
     @first_setting = Setting.first
-  end
-
-  def log 
-    render :temploate => 'home/log', :layout => 'plain'
-  end
-  
-  def moderate
-  end
-  
-  # iframe views
-
-  def chat
+    
+    # user name
     if params[:user_name] 
       @user_name = params[:user_name]
     else
@@ -25,10 +15,31 @@ class HomeController < ApplicationController
       @user_name = "Stammgast" if @user_name.nil?
     end
 
+    # avatar
 		params[:avatar_id] = 1 if params[:avatar_id].nil?
 		@avatar = Avatar.find_by_id(params[:avatar_id])
     
+    # moderator notice
     @chat_item = ChatItem.order("created_at").last
+    
+    @hide_pov = 1 if params[:hide_pov]
+    @hide_input = 1 if params[:hide_input]
+    @hide_notice = 1 if params[:hide_notice]
+  end
+
+  def log 
+    render :temploate => 'home/log', :layout => 'plain'
+  end
+  
+  def moderate
+    # allow moderation
+    @moderate = 1
+    render :template => 'home/chat'
+  end
+
+  def chat
+    # disallow moderation
+    @moderate = 0
   end
   
   
