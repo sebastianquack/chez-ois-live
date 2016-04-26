20df6f19176aaad058679854d5efc883bf73ca77
Sebastian Quack <sebastianquack@gmail.com>
2016-04-26 10:58:01 +0200

    remove places

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,49 +8,11 @@ class HomeController < ApplicationController
     @first_setting = Setting.first
   end
 
-	def landing
-		@avatars = Avatar.all		
-		@user_name = cookies[:user_name]
-		@user_name = "Stammgast" if @user_name.nil?
-
-		@settings = Setting.first
-		
-		if @settings.redirect == 1 && !params[:redirect_off]
-			redirect_to @settings.redirect_to
-		end
-
-	end
-
-	def play
-		@user_hash = cookies[:user_hash]
-		@user_hash = cookies.permanent[:user_hash] = SecureRandom.uuid if @user_hash.nil?
-		
-		@user_name = cookies[:user_name]
-		@user_name = "Stammgast" if @user_name.nil?
-
-		params[:place_id] = 1 if params[:place_id].nil?
-		@place = Place.find_by_id(params[:place_id])
-		
-		params[:avatar_id] = 1 if params[:avatar_id].nil?
-		@avatar = Avatar.find_by_id(params[:avatar_id])
-	end
-
-	def terminal
-		@user_hash = cookies[:user_hash]
-		@user_hash = cookies.permanent[:user_hash] = SecureRandom.uuid if @user_hash.nil?
-		
-		@user_name = cookies[:user_name]
-		@user_name = "Stammgast" if @user_name.nil?
-
-		@avatars = Avatar.all		
-		@avatar = Avatar.find_by_id(params[:avatar_id])
-		@place = Place.find_by_id(params[:place_id])
-
-		render :template => 'home/terminal', :layout => 'terminal'
-	end
-  
   def log 
     render :temploate => 'home/log', :layout => 'plain'
+  end
+  
+  def moderate
   end
   
   # iframe views
@@ -68,16 +30,6 @@ class HomeController < ApplicationController
     
     @chat_item = ChatItem.order("created_at").last
   end
-
-  def highscores
-		params[:avatar_id] = 1 if params[:avatar_id].nil?
-		@avatar = Avatar.find_by_id(params[:avatar_id])
-  end
   
-  def moderate
-  end
-  
-  def test
-  end
   
 end
--- app/models/place.rb
+++
@@ -1,4 +0,0 @@
-class Place < ActiveRecord::Base
-  #attr_accessible :fix_stream_embed, :fix_stream_embed_local, :name
-  has_many :avatars
-end
