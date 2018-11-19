a17008555ca8643ba492e7c0b76333bf3b88cb7e
Holger H <holgheiss@gmail.com>
2018-11-19 17:27:23 +0100

    schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -17,14 +17,14 @@ ActiveRecord::Schema.define(version: 20180426131312) do
   enable_extension "plpgsql"
 
   create_table "avatars", force: :cascade do |t|
-    t.string   "name"
+    t.string   "name",                   limit: 255
     t.text     "pov_stream_embed"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                          null: false
+    t.datetime "updated_at",                                          null: false
     t.integer  "place_id"
     t.text     "pov_stream_embed_local"
-    t.string   "pushover_user_key"
-    t.string   "gender",                 default: "male"
+    t.string   "pushover_user_key",      limit: 255
+    t.string   "gender",                 limit: 255, default: "male"
     t.string   "custom_css"
     t.string   "prompt"
     t.string   "input_placeholder"
@@ -32,86 +32,86 @@ ActiveRecord::Schema.define(version: 20180426131312) do
 
   create_table "chat_items", force: :cascade do |t|
     t.text     "content"
-    t.string   "ip_address"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "name"
-    t.string   "time_string"
+    t.string   "ip_address",  limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.string   "name",        limit: 255
+    t.string   "time_string", limit: 255
   end
 
   create_table "ip_blacklists", force: :cascade do |t|
-    t.string   "ip_address"
-    t.string   "user_name"
+    t.string   "ip_address", limit: 255
+    t.string   "user_name",  limit: 255
     t.integer  "status"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   add_index "ip_blacklists", ["ip_address"], name: "index_ip_blacklists_on_ip_address", unique: true, using: :btree
 
   create_table "places", force: :cascade do |t|
     t.text     "fix_stream_embed"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "name"
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.string   "name",                   limit: 255
     t.text     "fix_stream_embed_local"
   end
 
   create_table "settings", force: :cascade do |t|
-    t.string   "redirect_to"
+    t.string   "redirect_to",                      limit: 255
     t.integer  "redirect"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                                                                  null: false
+    t.datetime "updated_at",                                                                                  null: false
     t.integer  "timeout"
     t.string   "custom_css"
-    t.integer  "num_display_suggestions",          default: 4
-    t.string   "moderator_token",                  default: "1ckemod"
-    t.string   "default_username",                 default: "Guest"
-    t.string   "local_initial_greeting",           default: "Hi"
-    t.string   "local_name_change_button",         default: "ändern"
-    t.string   "local_name_change_prompt",         default: "Wie willst du heißen"
-    t.string   "local_name_change_confirm",        default: "Ok"
-    t.string   "local_suggestion_transmit_notice", default: "wird übertragen..."
-    t.string   "local_upvote_button",              default: "△ Pro"
-    t.string   "local_downvote_button",            default: "▽ Contra"
-    t.string   "local_upvote_count",               default: "Pros"
-    t.string   "local_downvote_count",             default: "Contras"
-    t.string   "local_text_to_speach_says",        default: "sagt"
-    t.string   "local_max_char",                   default: "Maximum length 80 characters."
-    t.string   "local_max_suggest",                default: "Too many submissions. Please wait."
+    t.integer  "num_display_suggestions",                      default: 4
+    t.string   "moderator_token",                              default: "1ckemod"
+    t.string   "default_username",                             default: "Guest"
+    t.string   "local_initial_greeting",                       default: "Hi"
+    t.string   "local_name_change_button",                     default: "ändern"
+    t.string   "local_name_change_prompt",                     default: "Wie willst du heißen"
+    t.string   "local_name_change_confirm",                    default: "Ok"
+    t.string   "local_suggestion_transmit_notice",             default: "wird übertragen..."
+    t.string   "local_upvote_button",                          default: "△ Pro"
+    t.string   "local_downvote_button",                        default: "▽ Contra"
+    t.string   "local_upvote_count",                           default: "Pros"
+    t.string   "local_downvote_count",                         default: "Contras"
+    t.string   "local_text_to_speach_says",                    default: "sagt"
+    t.string   "local_max_char",                               default: "Maximum length 80 characters."
+    t.string   "local_max_suggest",                            default: "Too many submissions. Please wait."
   end
 
   create_table "suggestions", force: :cascade do |t|
     t.text     "content"
-    t.string   "name"
+    t.string   "name",              limit: 255
     t.integer  "score"
     t.integer  "status"
-    t.string   "ip_address"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "user_hash"
+    t.string   "ip_address",        limit: 255
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
+    t.string   "user_hash",         limit: 255
     t.integer  "avatar_id"
-    t.string   "time_string"
+    t.string   "time_string",       limit: 255
     t.integer  "voting_started_at"
-    t.string   "name2"
-    t.boolean  "read",              default: false
+    t.string   "name2",             limit: 255
+    t.boolean  "read",                          default: false
   end
 
   create_table "user_scores", force: :cascade do |t|
-    t.string   "user_name"
-    t.string   "user_hash"
+    t.string   "user_name",  limit: 255
+    t.string   "user_hash",  limit: 255
     t.integer  "score"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.integer  "avatar_id"
   end
 
   create_table "user_votes", force: :cascade do |t|
     t.integer  "suggestion_id"
-    t.string   "user_hash"
+    t.string   "user_hash",     limit: 255
     t.integer  "vote"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
 end
